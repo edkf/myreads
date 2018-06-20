6ce68275cf6ebce8f204e0e8b39a4b21772a3da4
Edgard Kozlowski <edgardfernandes11@gmail.com>
2018-06-19 23:59:49 -0300

    :recycle: change router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 import './components/global.js'
 import Provider from './components/Provider'
@@ -12,34 +12,32 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
-        <Provider>
-          <React.Fragment>
-            <SearchBar />
-            <Header />
-            <Context.Consumer>
-              {(context) => {
+      <Provider>
+        <React.Fragment>
+          <SearchBar />
+          <Header />
+          <Context.Consumer>
+            {(context) => {
 
-                const { books, queriedBooks } = context.state
+              const { books, queriedBooks } = context.state
 
-                const reading = books.filter((book) => book.shelf === 'currentlyReading')
-                const toRead = books.filter((book) => book.shelf === 'wantToRead')
-                const completed = books.filter((book) => book.shelf === 'read')
+              const reading = books.filter((book) => book.shelf === 'currentlyReading')
+              const toRead = books.filter((book) => book.shelf === 'wantToRead')
+              const completed = books.filter((book) => book.shelf === 'read')
 
-                return (
-                  <React.Fragment>
-                    <Route exact path='/' render={(props) => <Bookshelf books={books} />} />
-                    <Route exact path="/reading" render={(props) => <Bookshelf books={reading} />} />
-                    <Route exact path="/to-read" render={(props) => <Bookshelf books={toRead} />} />
-                    <Route exact path="/completed" render={(props) => <Bookshelf books={completed} />} />
-                    <Route exact path="/search" render={(props) => <Bookshelf books={queriedBooks || []} />} />
-                  </React.Fragment>
-                )
-              }}
-            </Context.Consumer>
-          </React.Fragment>
-        </Provider>
-      </Router>
+              return (
+                <React.Fragment>
+                  <Route exact path='/' render={(props) => <Bookshelf books={books} />} />
+                  <Route exact path="/reading" render={(props) => <Bookshelf books={reading} />} />
+                  <Route exact path="/to-read" render={(props) => <Bookshelf books={toRead} />} />
+                  <Route exact path="/completed" render={(props) => <Bookshelf books={completed} />} />
+                  <Route exact path="/search" render={(props) => <Bookshelf books={queriedBooks || []} />} />
+                </React.Fragment>
+              )
+            }}
+          </Context.Consumer>
+        </React.Fragment>
+      </Provider>
     )
   }
 }
