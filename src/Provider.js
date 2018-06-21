cca7a056c63a59208d031bbe26741e23893137c4
Edgard Kozlowski <edgardfernandes11@gmail.com>
2018-06-21 00:16:17 -0300

    :bug: Check if query.length > 0 to prevent error 403

--- src/Provider.js
+++ src/Provider.js
@@ -27,7 +27,7 @@ class Provider extends Component {
           this.setState({
             query
           })
-          if (query) {
+          if (query  && query.length > 1) {
             BooksAPI.search(this.state.query).then((queriedBooks) => {
               if (queriedBooks && queriedBooks.length > 0) {
                 queriedBooks.map((queriedBook) => {
